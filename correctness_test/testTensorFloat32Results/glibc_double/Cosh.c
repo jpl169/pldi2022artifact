11d72ae24880a23a482da58311f1df2b332a0712
Jay Lim <jay.lim@yale.edu>
2022-02-27 17:13:50 -0500

    crlibm

--- correctness_test/testTensorFloat32Results/glibc_double/Cosh.c
+++ correctness_test/testTensorFloat32Results/glibc_double/Cosh.c
@@ -1,5 +1,12 @@
 #define __GEN_ELEM__ rlibm34_cosh
-#define	__ELEM__ cosh
+#define	__ELEM_TF32__ cosh
+#define	__ELEM_BF16__ cosh
+#define __ELEM_FP32_SAME__ 1
+#define __ELEM_FP32_RNE__ cosh
+#define __ELEM_FP32_RNA__ cosh
+#define __ELEM_FP32_RNZ__ cosh
+#define __ELEM_FP32_RNP__ cosh
+#define __ELEM_FP32_RNN__ cosh
 #include "LibTestHelper.h"
 
 int main(int argc, char** argv) {
--- correctness_test/testTensorFloat32Results/glibc_double/Log2.c
+++ correctness_test/testTensorFloat32Results/glibc_double/Log2.c
@@ -1,5 +1,12 @@
 #define __GEN_ELEM__ rlibm34_log2
-#define	__ELEM__ log2
+#define	__ELEM_TF32__ log2
+#define	__ELEM_BF16__ log2
+#define __ELEM_FP32_SAME__ 1
+#define __ELEM_FP32_RNE__ log2
+#define __ELEM_FP32_RNA__ log2
+#define __ELEM_FP32_RNZ__ log2
+#define __ELEM_FP32_RNP__ log2
+#define __ELEM_FP32_RNN__ log2
 #include "LibTestHelper.h"
 
 int main(int argc, char** argv) {
