fb1755d1f67670ddca9769a4b02919a3255d2bde
Jay Lim <jay.lim@yale.edu>
2022-02-27 18:25:39 -0500

    crlibm

--- correctness_test/testTensorFloat32Results/LibTestHelper.h
+++ correctness_test/testTensorFloat32Results/LibTestHelper.h
@@ -21,7 +21,7 @@ void RunTestForExponent() {
   
   floatX fx;
   
-  for (unsigned long count = 0x0; count < 0x100000000; count += 0x80) {
+  for (unsigned long count = 0x0; count < 0x100000000; count += 0x1) {
     fx.x = count;
     double orc = __GEN_ELEM__(fx.f);
 
