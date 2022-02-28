e3697270cae3a4124f33ae1c5076f28954df8667
Jay Lim <jay.lim@yale.edu>
2022-02-28 14:36:25 -0500

    overhead wip

---
+++ overhead_test/runTest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd glibc_proglibm/bf16
+make -s clean
+make -s
+./runAll.sh
+cd ../tf32
+make -s clean
+make -s
+./runAll.sh
+cd ../rno
+make -s clean
+make -s
+./runAll.sh
+cd ../../glibc_double/bf16
+make -s clean
+make -s
+./runAll.sh
+cd ../tf32
+make -s clean
+make -s
+./runAll.sh
+cd ../rno
+make -s clean
+make -s
+./runAll.sh
+cd ../..
