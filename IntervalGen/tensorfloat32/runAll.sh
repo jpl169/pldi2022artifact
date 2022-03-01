938e4151196016762ddf97233a8ff31015871ba6
Jay Lim <jay.lim@yale.edu>
2022-03-01 16:42:51 -0500

    remove unnecessary fiels

--- IntervalGen/tensorfloat32/runAll.sh
+++ IntervalGen/tensorfloat32/runAll.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 mkdir -p ${TF32INTERVALPATH}
 
-./Log ${TF32INTERVALPATH}/bf16LogIntervals
-./Log2 ${TF32INTERVALPATH}/bf16Log2Intervals
-./Log10 ${TF32INTERVALPATH}/bf16Log10Intervals
-./Exp ${TF32INTERVALPATH}/bf16ExpIntervals
-./Exp2 ${TF32INTERVALPATH}/bf16Exp2Intervals
-./Exp10 ${TF32INTERVALPATH}/bf16Exp10Intervals
-./Sinh ${TF32INTERVALPATH}/bf16SinhForSinhIntervals ${TF32INTERVALPATH}/bf16CoshForSinhIntervals
-./Cosh ${TF32INTERVALPATH}/bf16SinhForCoshIntervals ${TF32INTERVALPATH}/bf16CoshForCoshIntervals
-./Sinpi ${TF32INTERVALPATH}/bf16SinpiForSinpiIntervals ${TF32INTERVALPATH}/bf16CospiForSinpiIntervals
-./Cospi ${TF32INTERVALPATH}/bf16SinpiForCospiIntervals ${TF32INTERVALPATH}/bf16CospiForCospiIntervals
+./Log ${TF32INTERVALPATH}/tf32LogIntervals
+./Log2 ${TF32INTERVALPATH}/tf32Log2Intervals
+./Log10 ${TF32INTERVALPATH}/tf32Log10Intervals
+./Exp ${TF32INTERVALPATH}/tf32ExpIntervals
+./Exp2 ${TF32INTERVALPATH}/tf32Exp2Intervals
+./Exp10 ${TF32INTERVALPATH}/tf32Exp10Intervals
+./Sinh ${TF32INTERVALPATH}/tf32SinhForSinhIntervals ${TF32INTERVALPATH}/tf32CoshForSinhIntervals
+./Cosh ${TF32INTERVALPATH}/tf32SinhForCoshIntervals ${TF32INTERVALPATH}/tf32CoshForCoshIntervals
+./Sinpi ${TF32INTERVALPATH}/tf32SinpiForSinpiIntervals ${TF32INTERVALPATH}/tf32CospiForSinpiIntervals
+./Cospi ${TF32INTERVALPATH}/tf32SinpiForCospiIntervals ${TF32INTERVALPATH}/tf32CospiForCospiIntervals
 
